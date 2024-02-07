090418232044d9c8780a57788b575eb29b90b1e7
Joao <joao.rampoo@gmail.com>
2024-02-07 17:29:23 -0300

    feat: footer + media fix

--- src/main.js
+++ src/main.js
@@ -22,7 +22,8 @@ function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open'
     const elementoPai = elemento.target.parentNode
 
-    elementoPai.classList.toggle(classe)
+    elementoPai.classList.toggle(classe)     
+    
 }
 
 function removeBotaoAtivo() {
